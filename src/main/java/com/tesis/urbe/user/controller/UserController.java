6aa78a8fb7f03ab7c80e247d08c3a3cff5eb42b8
ManuDev01 <bymadby80@gmail.com>
2026-06-29 21:26:32 -0400

    no se

--- src/main/java/com/tesis/urbe/user/controller/UserController.java
+++ src/main/java/com/tesis/urbe/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.tesis.urbe.user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tesis.urbe.user.service.UserService;
@@ -8,4 +10,11 @@ import com.tesis.urbe.user.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public ResponseEntity<?> getUsers() {
+        return this.
+    };
 }
